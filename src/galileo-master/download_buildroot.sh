9c253cb7e163ceecc0d85feb6698296866680cd0
aritzelge <aritzelge@Ubuntu-16.04.5-i386.myguest.virtualbox.org>
2025-11-25 22:04:50 +0100

    chore(workspace): Define workspace structure and add working setup scripts

---
+++ src/galileo-master/download_buildroot.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# --- Script Configuration ---
+BUIDLROOT_VERSION=2019.02
+REPO_URL="git://git.buildroot.net/buildroot.git"
+
+# Install Buildroot dependencies
+
+sudo ./install_dependencies.sh
+EXIT_STATUS=$?
+
+# Controll return from script
+if [ $EXIT_STATUS -ne 0 ]; then
+	echo "[ERROR] The dependency installation script failed (Exit Status: 1)"
+	echo "[ERROR] Cannot proceed with Buildroot setup. Exiting"
+	exit 1
+fi
+
+echo "[INFO] Dependency installation finished successfully. Proceeding with Buildroot setup."
+
+# Change directory to ensure the scripts runs from the galileo-master/ root
+cd "$(dirname "$0")"
+
+echo " Starting Buildroot download for Galileo Gen 2"
+
+# -- Step1: Clone the Buildroot repository --
+if [ ! -d ".git" ]; then
+	echo "[INFO] .git directory not found. Cloning repository..."
+	git clone "$REPO_URL"
+	if [ $? -eq 0 ]; then
+		echo "[INFO] Cloning completed successfully."
+	else
+		echo "[ERROR] Failed during repository cloning. Exiting."
+		exit 1
+	fi
+else
+	echo "[INFO] Repository already cloned. Skipping cloning step"
+fi
+
+# -- Step 2: Switch to the specific version (tag) --
+echo "[INFO] Switching to the specific version (tag): 2019.02"
+cd buildroot
+git checkout "${BUIDLROOT_VERSION}"
+if [ $? -eq 0 ]; then
+	echo "[INFO] Checkout completed successfully. version 2019.02 selected."
+else
+	echo "[ERROR] Failed to checkout version 2019.02, Exiting."
+	exit 1
+fi
+
+echo "Buildroot configuration complete."
+echo "Now, run 'make galileo_defconfig' , 'make menuconfig', 'make linux-menuconfig' and 'make' in the 'buildroot/' directory."
+
+exit 0
---
+++ src/galileo-master/install_dependencies.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Installing Host dependencies for Buildroot"
+
+# Initialize the error flag: 0 = success; 1 = failure
+INSTALL_FAILED=0
+
+# Check if the user has root privileges to run apt install
+if [ "$(id -u)" != "0" ]; then
+	echo "[WARNING] This script needs root privileges to install packages!"
+	echo "[WARNING] Please run this script with 'sudo ./install_dependencies.sh"
+	INSTALL_FAILED =1
+	exit $INSTALL_FAILED
+fi
+
+echo "[INFO] Updating package list..."
+sudo apt update
+
+# Install the dependencies
+sudo apt install -y build-essential libncurses5-dev libncursesw5-dev flex bison libssl-dev libelf-dev git
+
+INSTALL_FAILED=$?
+
+if [ $INSTALL_FAILED -eq 0 ]; then
+	echo "[INFO] All dependencies installed successfully."
+else
+	echo "[WARNING] Dependency installation completed with errors"
+	echo "[WARNING] Please review the output for failed packages"
+fi
+
+# Exit with the flag value (0 for success, 1 for failure)
+
+exit $INSTALL_FAILED
