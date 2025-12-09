d6151c87d180aee03730c32e7f1d596571d931d6
AritzElge <aelguezabal010@gmail.com>
2025-12-09 21:23:09 +0100

    fix: Fixed path calls

--- scripts/copy_daemons.sh
+++ scripts/copy_daemons.sh
@@ -13,19 +13,19 @@ mkdir -p "$OVERLAY_USR_SBIN_PATH" || exit 1
 # Copy the daemons to the destination directory.
 # 1. error_code_blink daemon
 echo "[DAEMON_COPY] Copying error_code_blink daemon..."
-cp -v $(DAEMONS_PATH)/error_code_blink/error_code_blink $(OVERLAY_USR_SBIN_PATH)/error_code_blink || exit 1
+cp -v "${DAEMONS_PATH}"/error_code_blink/error_code_blink "${OVERLAY_USR_SBIN_PATH}"/error_code_blink || exit 1
 # Ensure execute permissions:
-chmod +x $(OVERLAY_USR_SBIN_PATH)/error_code_blink || exit 1
+chmod +x "${OVERLAY_USR_SBIN_PATH}"/error_code_blink || exit 1
 # 2. ssh_app
 echo "[DAEMON_COPY] Copying ssh_app (not an actual daemon)..."
-cp -rv $(DAEMONS_PATH)/ssh_app/* $(OVERLAY_USR_BIN_PATH)/ || exit 1
+cp -rv "${DAEMONS_PATH}"/ssh_app/* "${OVERLAY_USR_BIN_PATH}"/ || exit 1
 # Ensure execute permissions:
-chmod +x $(OVERLAY_USR_BIN_PATH)/monitor_status.sh || exit 1
+chmod +x "${OVERLAY_USR_BIN_PATH}"/monitor_status.sh || exit 1
 # 3. modbus daemons
 echo "[DAEMON_COPY] Copying modbus daemons..."
-cp -v $(DAEMONS_PATH)/modbus/* $(OVERLAY_USR_BIN_PATH)/ || exit 1
+cp -v "${DAEMONS_PATH}"/modbus/* "${OVERLAY_USR_BIN_PATH}"/ || exit 1
 # Ensure execute permissions:
-chmod +x $(OVERLAY_USR_BIN_PATH)/start_polling_daemon.sh || exit 1
-chmod +x $(OVERLAY_USR_BIN_PATH)/start_schedule_daemon.sh || exit 1
+chmod +x "${OVERLAY_USR_BIN_PATH}"/start_polling_daemon.sh || exit 1
+chmod +x "${OVERLAY_USR_BIN_PATH}"/start_schedule_daemon.sh || exit 1
 
 echo "Copy completed."
